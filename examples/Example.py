cc2a3717abee943e2f1ac62f2b8cc96f5b959650
João Sartori <53714290+jsartori12@users.noreply.github.com>
2026-03-17 19:18:37 -0300

    Modify interface metrics and add DMS parallel execution
    Updated Get_Interface_descriptors to include minimize parameter and added Run_DMS_Parallel function call with parameters.

--- examples/Example.py
+++ examples/Example.py
@@ -15,4 +15,20 @@ pdbace2rbd = "RBD_ACE2.pdb_relax.pdb"
 by_term_by_residue = utils_pyrosetta.Energy_contribution(pdb = pdbace2rbd, by_term=True)
 sumofterms_by_residue = utils_pyrosetta.Energy_contribution(pdb = pdbace2rbd, by_term=False)
 
-Interface_metrics = utils_pyrosetta.Get_Interface_descriptors(pdb = pdbace2rbd, partner1 = "A", partner2 = "D")
+
+Interface_metrics = utils_pyrosetta.Get_Interface_descriptors(pdb = pdbace2rbd, partner1 = "A", partner2 = "D", minimize=False)
+
+
+df_dms = utils_pyrosetta.Run_DMS_Parallel(
+    pdb=pdbace2rbd,
+    positions=[300,311,312,313],
+    n_cpu=4,
+    chain = "D",
+    save_structures=True,          # dump PDB for every mutant
+    output_dir="./DMS_results",
+    fast_relax_repeats=0,          # apply 1 FastRelax round per mutant
+)
+
+utils_pyrosetta.plot_dms_heatmap(df_dms)
+
+
